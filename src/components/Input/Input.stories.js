a68a71b141a22d133a0715f3cb52807f8680b8cd
Wojcikowski Frederic <wojcikowskif@gmail.com>
2021-05-04 16:29:15 +0900

    story heirarchy

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -3,10 +3,12 @@ import React from 'react';
 import Input from './Input';
 
 export default {
-  title: 'Input',
+  title: 'Form/Input',
   component: Input,
 };
 
 export const Small = () => <Input size="small" placeholder="Small Size" />;
 export const Medium = () => <Input size="medium" placeholder="Medium Size" />;
 export const Large = () => <Input size="large" placeholder="Large Size" />;
+
+Small.storyName = 'Small Input';
